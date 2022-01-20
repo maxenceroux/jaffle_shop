27981c507540c3afaf96d2cb1baf38cfa140ef35
maxenceroux <maxence.roux@ornikar.com>
2022-01-20 16:57:56 +0100

    Fix typo

--- models/meeting_points.sql
+++ models/meeting_points.sql
@@ -18,5 +18,5 @@ with meeting_points as (
   cast(gearbox_type as int) as gearbox_type
 FROM
   `data-265818.airbyte_fivetran_test_migration.meeting_points`
-LIMIT
-  1000
+) 
+select * from meeting_points
